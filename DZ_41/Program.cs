c47b7a665d68542674bbca7080c873377d150a20
Nels <boldirevpavel97@yandex.ru>
2022-10-24 21:48:29 +0300

    add DZ41_43

---
+++ DZ_41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Task 41. User enter number M. Count, hom much numbers > 0 user entered.
+
+Console.Clear();
+
+Console.WriteLine("Enter count of numbers: ");
+int count = int.Parse(Console.ReadLine() ?? "");
+
+int [] numbers = new int [count];
+
+Console.Write($"Колличество положительных чисел : {HowMuchPositive(numbers)}");
+
+// ------------------Logic-------------------------------------------------------
+int HowMuchPositive(int[] numbers) //Метод подсчитывает и возвращает кол-во чисел больше 0
+{
+int counters = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine("Enter number: ");
+    numbers[i] = int.Parse(Console.ReadLine() ?? "");
+    if (numbers[i] > 0) counters += 1;
+}
+return counters;
+}
---
+++ DZ_43/Program.cs
@@ -0,0 +1,44 @@
+﻿// Task 43. Write programm that finds the intersection point of two staight lines
+// y = k1 * x + b1, y = k2 * x + b2;   b1, k1, b2, k2 - user inputs
+
+Console.Clear();
+
+//точка пересечения - решить систеу из двух уравнений, выразить х через b1 и b2 и ее 
+//подставить хоть в 1 хоть во 2 уравнение, тем самым получим y
+
+double [] numbers = new double [4];
+FillArray(numbers);
+
+Coordination(numbers);
+
+// ------------------Logic-------------------------------------------------------
+double[] FillArray(double[] numbers) //Заполняем массив введеными числами
+{
+    for (int i = 0; i < 4; i++)
+    {
+    Console.WriteLine("Введите число: ");
+    numbers[i] = double.Parse(Console.ReadLine() ?? "");
+    }
+    return numbers;
+}
+
+void Coordination(double[] numbers) //Находим точку пересечения
+{
+    double y = 0;
+    double x = 0;
+    double i = -100;
+    while (i < 100)
+    {       //решение системы уравнений 2 5 4 9
+            // y = b1 * x + k1    2x +  5     -2x 4 x = -2
+            // y = b2 * x + k2    4x +  9
+    if ((numbers[0] * i) + numbers[1] == (numbers[2] * i) + numbers[3])
+        Console.WriteLine(i);
+        x = i;
+        y = (numbers[2] * x) + numbers[3];       
+        Console.WriteLine($"b1 = {numbers[0]}, k1 = {numbers[1]}, b2 = {numbers[2]}, k2 = {numbers[3]} -> ({x};{y})");
+        break;
+    }
+}
+
+//Не получилось(
+
